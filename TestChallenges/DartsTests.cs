2e2658edb2e57ed1484b09d167ad945f704720ca
Najse <34438787+Najse@users.noreply.github.com>
2020-11-05 17:38:58 +0100

    Darts done, all tests passed, fuck this challenge

---
+++ TestChallenges/DartsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestingStuff.Challenges;
+
+
+namespace TestChallenges
+{
+    [TestFixture]
+    class DartsTests
+    {
+        [TestCase(new int[] { 3, 6, 8, 11, 15, 19, 22 }, 3, 35, ExpectedResult = new string[] { "8-8-19" })]
+        [TestCase(new int[] { 2, 4, 7, 10, 13, 18, 24 }, 3, 29, ExpectedResult = new string[] { "4-7-18" })]
+        [TestCase(new int[] { 3, 7, 11, 14, 18, 20, 25 }, 3, 40, ExpectedResult = new string[] { "11-11-18" })]
+        [TestCase(new int[] { 3, 7, 11, 14, 18, 20, 25 }, 3, 8, ExpectedResult = new string[] { })]
+        [TestCase(new int[] { 3, 7, 11, 14, 18, 20, 25 }, 3, 32, ExpectedResult = new string[] { "3-11-18", "7-7-18", "7-11-14" })]
+        [TestCase(new int[] { 3, 7, 11, 14, 18, 20, 25, 29, 34 }, 3, 67, ExpectedResult = new string[] { "18-20-29" })]
+        [TestCase(new int[] { 3, 7, 11, 14, 18, 20, 25 }, 4, 23, ExpectedResult = new string[] { "3-3-3-14" })]
+        public static string[] TestDartsSolver(int[] sections, int darts, int target)
+        {
+            return Darts.DartsSolver(sections, darts, target);
+        }
+    }
+}
---
+++ TestingStuff/Challenges/Darts.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestingStuff.Models;
+
+namespace TestingStuff.Challenges
+{
+    //Fuck this challenge
+    public class Darts : IChallenge
+    {
+        public static string[] DartsSolver(int[] sections, int darts, int target)
+        {
+            var once = new List<int>();
+            var twice = new List<int>();
+
+            for (var i = 0; i < sections.Length; i++)
+            {
+                for (var j = 0; j < sections.Length; j++)
+                {
+                    if (j == i) continue;
+                    if (!sections.Contains(target - (sections[i] + sections[j]))) continue;
+                    once.Add(sections[i]);
+                    once.Add(sections[j]);
+                }
+
+                if (!sections.Contains(target - (sections[i] * (darts - 1)))) continue;
+                twice.Add((target - (sections[i] * (darts - 1))));
+                for (var x = 0; x < darts - 1; x++)
+                    twice.Add(sections[i]);
+            }
+
+            twice.Sort();
+            var distinctOne = once.Distinct();
+
+            var left = distinctOne.Take(darts).ToList();
+            left.Sort();
+
+            var right = distinctOne.Skip(darts).ToList();
+
+            right.Add(target - right.Sum());
+            right.Sort();
+
+            string toAddLeft = "", toAddRight = "", toAdd = "";
+            for (var i = 0; i < darts; i++)
+            {
+                if (left.Count == darts)
+                    toAddLeft += i <= 0 ? left[i].ToString() : "-" + left[i];
+                if (right.Count == darts)
+                    toAddRight += i <= 0 ? right[i].ToString() : "-" + right[i];
+                if (twice.Count == darts)
+                    toAdd += i <= 0 ? twice[i].ToString() : "-" + twice[i];
+            }
+
+
+            var solutions = new List<string>();
+
+            if (left.Count == darts)
+                solutions.Add(toAddLeft);
+            if (twice.Count == darts)
+                solutions.Add(toAdd);
+            if (right.Count == darts)
+                solutions.Add(toAddRight);
+
+
+            return solutions.ToArray();
+        }
+
+
+        public void Show()
+        {
+            DartsSolver(new int[] {3, 7, 11, 14, 18, 20, 25}, 3, 32); // {"3-11-18", "7-7-18", "7-11-14"}
+        }
+    }
+}
